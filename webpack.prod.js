7c515c722bfb37d091736c0ff3ad12aecc4fcd49
robin <476296987@qq.com>
2018-01-12 17:05:16 +0800

    update webpack plugin config

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,6 +3,8 @@ const merge = require('webpack-merge');
 
 const common = require('./webpack.common');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const ExtraTextPlugin = require('extract-text-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
     module: {
@@ -19,13 +21,22 @@ module.exports = merge(common, {
             },
             {
                 test: /\.css$/,
-                loaders: ['style-loader', 'css-loader']
+                use: ExtraTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: 'css-loader'
+                })
             }
         ]
     },
     devtool: 'cheap-module-source-map',
     plugins: [
-        new UglifyJSPlugin()
+        new UglifyJSPlugin(),
+        new ExtraTextPlugin('style.css'),
+        new HTMLWebpackPlugin({
+            filename: 'index.html',
+            inject: 'body',
+            template: './src/out.html'
+        })
     ],
     output: {
         path: path.resolve('dist'),
