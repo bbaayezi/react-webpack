928ff10b227b2c99625dcada5b43587c9bb43fc2
robin <476296987@qq.com>
2018-01-13 22:14:20 +0800

    add redux

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../jsx/App.jsx';
-import '../css/style.css'
+import '../css/style.css';
+
+import { createStore } from 'redux';
 
 ReactDOM.render(
     <App />,
